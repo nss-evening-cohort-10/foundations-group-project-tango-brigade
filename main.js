a41e876e28065ee2e2033cde78661a69360db65b
Kelsey Creel <kelseycreel@gmail.com>
2019-09-19 22:26:40 -0500

    merge from master

--- main.js
+++ main.js
@@ -128,36 +128,12 @@ const printEmailForm = () => {
     printToDom('email-zone', emailString);
 }
 
-// SHOW MORE BUTTON EVENT LISTENER
-//document.getElementById('show-more').addEventListener('click', printMessage);
-
-
 
 const checkPrev = () => {
     if (document.referrer.includes('index.html')) {
         logo.className += ' firstAnimation';
     } else {
         logo.className += ' leftToRight';
-    }
-}
-
-const pageCheck = () => {
-    if (currentPage.includes('tour.html')) {
-        tourDatePrinter(tourDates);
-        checkPrev();
-        printEmailForm();
-        document.getElementById('show-more').addEventListener('click', printMessage);
-        document.getElementById('subscribe').addEventListener('click', printNewMessage);
-
-    } else if (currentPage.includes('index.html')) {
-        logo.className - ' firstAnimation';
-
-    } else if (currentPage.includes('about.html')) {
-        checkPrev();
-        bandBuilder(bandMembers);
-        
-    } else if (currentPage.includes('disc.html')) {
-        checkPrev();    
     }
 }
 
@@ -177,14 +153,13 @@ const bandMembers = [
     image: '/images/seanmartin.jpg',
     name: 'Sean Martin',
     instrument: 'Bass Guitar'
-},{
+},
+{
     image: '/images/LiamLEstrange.jpg',
     name: "Liam L'Estrange",
     instrument: 'Drums'
-},
-    
+},  
 ]
-
 
 const bandBuilder = (bandArray) =>{
     let aboutBandCard='';
@@ -200,6 +175,128 @@ const bandBuilder = (bandArray) =>{
     }
     console.log("${bandcard.name}");
     printToDom('aboutMembersZone', aboutBandCard);
+}
+
+
+// DISC PAGE ARRAYS
+const tangoSingles = [
+    {
+        albumArt: 'https://img.discogs.com/K1D4-fJyXC7caO6HIUvBcLwmjNo=/fit-in/490x495/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3173532-1319058781.jpeg.jpg',
+        title: 'Donegal',
+        genre: 'Rock',
+        style: 'Power Pop, Punk',
+        year: '1981',
+        length: '2:33',
+    },
+    {
+        albumArt: 'https://img.discogs.com/Lmv4w_Eo8QrSkEfzDr72pPmlCc0=/fit-in/495x494/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3173532-1319058788.jpeg.jpg',
+        title: 'In Vain',
+        genre: 'Rock',
+        style: 'Power Pop, Punk',
+        year: '1981',
+        length: '3:58',
+    },
+]
+
+const starjetsSingles = [
+    {
+        albumArt: 'https://img.discogs.com/nVIPB0kOEPpOYzsEyDiREsKKMwI=/fit-in/600x588/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2763503-1454090346-5512.jpeg.jpg',
+        title: 'Shiraleo',
+        genre: 'Rock',
+        style: 'New Wave, Punk',
+        year: '1980',
+        length: '3:05',
+    },
+    {
+        albumArt: 'https://img.discogs.com/8wYrtsrCchKeOcRj8l8tX40_tzA=/fit-in/389x392/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2763518-1363288596-1803.png.jpg',
+        title: 'Schooldays',
+        genre: 'Rock',
+        style: 'Punk',
+        year: '1979',
+        length: '3:10',
+    },
+    {
+        albumArt: 'https://img.discogs.com/OGMaZ-1BItKlgGQShGlCrMbrFqE=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2187103-1560481243-4479.jpeg.jpg',
+        title: 'War Stories',
+        genre: 'Rock',
+        style: 'Power Pop',
+        year: '1979',
+        length: '3:12',
+    },
+    {
+        albumArt: 'https://img.discogs.com/yTV9GjghvGaYtE5r9jL7X9yDe08=/fit-in/600x598/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1404033-1560480775-8379.jpeg.jpg',
+        title: 'Ten Years',
+        genre: 'Rock',
+        style: 'New Wave, Power Pop',
+        year: '1979',
+        length: '3:20',
+    },
+    {
+        albumArt: 'https://img.discogs.com/dd616C9hAiVRWxPLnLMYSFj1Fkw=/fit-in/600x609/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-11717726-1521197245-6762.jpeg.jpg',
+        title: "It Really Doesn't Matter",
+        genre: 'Rock',
+        style: 'Pop Punk',
+        year: '1979',
+        length: '3:07',
+    },
+    {
+        albumArt: 'https://img.discogs.com/OOvU2azluPFYnd4v_MRhll5Ls8o=/fit-in/591x593/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2244745-1392388032-1911.jpeg.jpg',
+        title: 'Here She Comes Again',
+        genre: 'Rock',
+        style: 'Punk',
+        year: '1978',
+        length: '3:22',
+    },
+];
+
+const starjetsAlbums = [
+    {
+        albumArt: 'https://img.discogs.com/xubyZAIv_YPT8t4J9Mx9VNc4ohw=/fit-in/600x593/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3521390-1398544495-9162.jpeg.jpg',
+        albumTitle: 'God Bless Starjets: The Punk Collection',
+        albumYear: '1999',
+        record: 'Epic',
+    },
+    {
+        albumArt: 'https://img.discogs.com/LQjx89380Xp-Ok74uhxtZsz9opA=/fit-in/600x584/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10949196-1522281917-2768.jpeg.jpg',
+        albumTitle: 'The 1979 Belfast Demo Session Volume II',
+        albumYear: '1979, 2018',
+        record: 'Spit Records',
+    },
+    {
+        albumArt: 'https://img.discogs.com/y0Q_1EcsP9x08RjQZPn33GhN0DM=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10637314-1521049514-9146.jpeg.jpg',
+        albumTitle: 'The 1979 Belfast Demo Session Volume I',
+        albumYear: '1979, 2018',
+        record: 'Spit Records',
+    },
+    {
+        albumArt: 'https://img.discogs.com/KEAhNtDSz7x6xJcVgiuimJFG5Ng=/fit-in/600x605/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-1321394-1564910928-8348.jpeg.jpg',
+        albumTitle: 'God Bless Starjets',
+        albumYear: '1979',
+        record: 'Epic',
+    },
+]
+
+
+
+// PAGE LOAD CHECK DO NOT PUT ANYTHING BELOW THIS
+const pageCheck = () => {
+    if (currentPage.includes('tour.html')) {
+        tourDatePrinter(tourDates);
+        checkPrev();
+        printEmailForm();
+        document.getElementById('show-more').addEventListener('click', printMessage);
+        document.getElementById('subscribe').addEventListener('click', printNewMessage);
+
+    } else if (currentPage.includes('index.html')) {
+        logo.className - ' firstAnimation';
+
+    } else if (currentPage.includes('about.html')) {
+        checkPrev();
+        bandBuilder(bandMembers);
+        
+    } else if (currentPage.includes('disc.html')) {
+        checkPrev();    
+    }
 }
 
 // INIT
